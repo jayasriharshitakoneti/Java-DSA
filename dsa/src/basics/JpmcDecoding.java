085b111ec91148ff2617849957aab93b55a78c78
Jaya Sriharshita Koneti <75335955+jayasriharshitakoneti@users.noreply.github.com>
2022-02-26 22:13:20 +0530

    Update JpmcDecoding.java

--- dsa/src/basics/JpmcDecoding.java
+++ dsa/src/basics/JpmcDecoding.java
@@ -8,14 +8,10 @@ public class JpmcDecoding {
 		String s = scan.nextLine();
 		int i = scan.nextInt();
 		double d = scan.nextDouble();
-		// String s = scan.nextLine();
-		// scan.nextLine();
-
-		// Write your code here.
-
-		System.out.println("String: " + s);
-		System.out.println("Double: " + d);
-		System.out.println("Int: " + i);
+		
+		System.out.printf("String: %s\n", s);
+		System.out.printf("Double: %f\n", d);
+		System.out.printf("Integer: %f\n", i);
 		scan.close();
 	}
 }
